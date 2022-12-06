30d429924acf5e5c610ee883e34d89fcc4f2166c
grass <81576338+Grassias10@users.noreply.github.com>
2022-12-06 07:37:47 -0500

    12/5 in class edits by riley and grass

--- monkeyType/script.js
+++ monkeyType/script.js
@@ -1,4 +1,8 @@
-class game{
+function waitForDelay(delay) {
+    return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+class Game{
     constructor(){
         this.wpm = 0;
         this.accuracy = 0;
@@ -6,6 +10,7 @@ class game{
         this.totalWordsTyped = 0;
         this.rightWordsTyped = 0;
         this.wrongWordsTyped = 0;
+        this.prompt = "";
     }
 
     calculateWPM(){
@@ -27,8 +32,9 @@ class game{
 
     generatePrompt(){
         /*
-        go through words_alpha and randomly choose 50 words
+        go through words_alpha and randomly choose 1000 words
         */
+
     }
 
     updateWordCount(){
@@ -37,9 +43,30 @@ class game{
         */
     }
 
-    isTypedRight(){
-        /*
-        checks if word is properly typed (case sensitive)
-        */
+    compareText(text) {
+        var textArr = text.split(" ");
+        var promptArr = prompt.split(" ");
+
+        this.totalWordsTyped = textArr.length;
+
+        for(var i = 0; i < textArr.legnth; i++){
+            if(textArr[i].toLowerCase() == promptArr[i].toLowerCase()){
+                this.rightWordsTyped++;
+            } else {
+                this.wrongWordsTyped++;
+            }
+        }
     }
+}
+
+async function startGame() {
+    var game = new Game();
+    game.generatePrompt();
+    var time = 0;
+    while (time < 30) {
+        await waitForDelay(1000);
+        time++;
+    }
+    var text = document.getElementById("textbox");
+    game.compareText(text);
 }
