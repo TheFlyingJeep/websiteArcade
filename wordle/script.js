a32ac83d70dc87ffbc42bf0efddb7b3a43b17591
rabelauren <86250825+rabelauren@users.noreply.github.com>
2022-12-09 11:59:27 -0500

    update script.js

---
+++ wordle/script.js
@@ -0,0 +1,6 @@
+function wordleRun(){
+    var userInput = document.getElementById("textInput").value;
+    
+    console.log(userInput);
+    document.getElementById("value").innerHTML = userInput;
+}
